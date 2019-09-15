c721f731d0f0b8a1b85435b3a0a6ee65475ae1c6
zacharyhosig <zacharyhosig@gmail.com>
2019-09-15 03:11:43 -0400

    all 4 pages setup correctly

---
+++ app/src/main/java/com/example/routerater/RoutePickerActivity.java
@@ -0,0 +1,14 @@
+package com.example.routerater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoutePickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_picker);
+    }
+}
